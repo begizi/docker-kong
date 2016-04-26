6a3ae40e4dd75434835fbf94c7b99065f66852c5
Brian Egizi <me@brianegizi.com>
2016-04-25 20:50:54 -0400

    Add custom hostname env variable

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sed -i "s/kongDatabase/$KONG_DATABASE_ENV_DOCKERCLOUD_NODE_HOSTNAME/g" /etc/kong/kong.yml
+
 # Setting up the proper database
 if [ -n "$DATABASE" ]; then
   echo -e '\ndatabase: "'$DATABASE'"' >> /etc/kong/kong.yml
